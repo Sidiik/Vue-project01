5adb3e11950d7272e19139dd6a9ea0c3802021de
Sadiq O <sadiqomar890@gmail.com>
2021-08-20 21:54:39 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "This is my last and first vue app",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.focus();
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <h1>Modal title</h1>
+      <p>Modal content here</p>
+      <button>close</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  height: 100vh;
+}
+.modal {
+  background-color: white;
+  width: 50%;
+  margin: auto;
+  padding: 2rem;
+  border-radius: 0.4rem;
+}
+h1 {
+  color: red;
+}
+button {
+  background: blue;
+  color: white;
+  padding: 0.3rem 1rem;
+  border: 0;
+  outline: 0;
+  border-radius: 0.2rem;
+  cursor: pointer;
+  font-size: 1rem;
+}
+</style>
